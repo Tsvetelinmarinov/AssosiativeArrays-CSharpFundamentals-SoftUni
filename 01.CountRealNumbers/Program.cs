08e159c94d4e9919aa956ce3fd7b4d44c4cc56ee
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-04 22:26:52 +0300

    Add project files.

---
+++ 01.CountRealNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * 01.CountRealNumbers
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace _01.CountRealNumbers;
+
+class Program
+{
+    static void Main()
+    {
+        List<double> numbers = Console.ReadLine()!
+            .Split()
+            .Select(double.Parse)
+            .ToList();
+
+        SortedDictionary<double, int> numberOccurences = new();
+        foreach (double number in numbers)
+        {
+            if (numberOccurences.ContainsKey(number))
+            {
+                numberOccurences[number]++;
+            }
+            else
+            {
+                numberOccurences.Add(number, 1);
+            }
+        }
+
+        foreach ((double number, int occurences) in numberOccurences)
+        {
+            Console.WriteLine($"{number} -> {occurences}");
+        }
+    }
+}
