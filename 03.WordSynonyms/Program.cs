e300cee4ec1fd5343a61a515c1486751940e9f02
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-05 14:54:33 +0300

    Задача 3

---
+++ 03.WordSynonyms/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * 03.WordSynonyms
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace _03.WordSynonyms;
+
+class Program
+{
+    static void Main()
+    {
+        int counter = int.Parse(Console.ReadLine()!);
+        Dictionary<string, List<string>> wordSynonyms = new();
+
+        for (; counter > 0; --counter)
+        {
+            string word = Console.ReadLine()!;
+            string synonym = Console.ReadLine()!;
+
+            if (wordSynonyms.ContainsKey(word))
+            {
+                wordSynonyms[word].Add(synonym);
+            }
+            else
+            {
+                wordSynonyms.Add(word, new List<string>()
+                {
+                    synonym
+                });
+            }
+        }
+
+        foreach (KeyValuePair<string, List<string>> pair in wordSynonyms)
+        {
+            Console.WriteLine($"{pair.Key} - {string.Join(", ", pair.Value)}");
+        }
+    }
+}
