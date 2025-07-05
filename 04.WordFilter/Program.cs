bf62b8ed25bdfd349b04bd270789821b9751cfa4
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-05 20:28:45 +0300

    Задача 4

---
+++ 04.WordFilter/Program.cs
@@ -0,0 +1,17 @@
+﻿///
+/// 04.WordFilter
+/// 
+
+namespace _04.WordFilter;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ReadLine()!
+            .Split()
+            .Where(fruit => fruit.Length % 2 == 0)
+            .ToList()
+            .ForEach(Console.WriteLine);
+    }
+}
