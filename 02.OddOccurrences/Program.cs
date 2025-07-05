2abaf657305e0c7fccffd6447c94a9affbf5fae8
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-05 14:12:14 +0300

    Задачи 1 и 2

---
+++ 02.OddOccurrences/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * 02.OddOccurrences
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OddOccurrences;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> words = Console.ReadLine()!
+            .Split()
+            .ToList();
+
+        Dictionary<string, int> wordOccurences = new();
+        foreach (string word in words)
+        {
+            if (wordOccurences.ContainsKey(word.ToLower()))
+            {
+                wordOccurences[word.ToLower()]++;
+            }
+            else
+            {
+                wordOccurences.Add(word.ToLower(), 1);
+            }
+        }
+
+        foreach ((string word, int occurence) in wordOccurences)
+        {
+            if (occurence % 2 != 0)
+            {
+                Console.Write($"{word} ");
+            }
+        }
+    }
+}
